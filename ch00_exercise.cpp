db0b916e886416173e51889c05fa5a5c33969e8a
Sage Ahn <sage@flyingloft.com>
2015-12-02 21:26:27 +0900

    Exercise 0-4 solved

--- ch00_exercise.cpp
+++ ch00_exercise.cpp
@@ -6,5 +6,11 @@ int main () {
 	std::cout << "\tA" << std::endl;
 	std::cout << "12345678901234" << std::endl;
 
+	std::cout << "#include <iostream>" << std::endl;
+	std::cout << std::endl;
+	std::cout << "int main () { " << std::endl;
+	std::cout << "\tstd::cout << \"Hello, World!\" << std::endl;" << std::endl;
+	std::cout << "}" << std::endl;
+
 	return 0;
 }
