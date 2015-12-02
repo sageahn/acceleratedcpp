c88fc7ca0c6832bee23ccc9cd8f1989a04819b49
Sage Ahn <sage@flyingloft.com>
2015-12-02 21:19:03 +0900

    Exercise 0-3 solved

--- ch00_exercise.cpp
+++ ch00_exercise.cpp
@@ -3,6 +3,8 @@
 int main () {
 	std::cout << "Hello, World!" << std::endl;
 	std::cout << "This (\") is a quote, and this (\\) is a backslash" << std::endl;
+	std::cout << "\tA" << std::endl;
+	std::cout << "12345678901234" << std::endl;
 
 	return 0;
 }
