46de29d9a3bc6caa27bd910ac9c8d7c1c88df368
Sage Ahn <sage@flyingloft.com>
2016-01-30 10:32:04 +0900

    현재 하는 일을 입력 받고 출력한다.

--- logger.cpp
+++ logger.cpp
@@ -1,10 +1,19 @@
 // logger main
+//
+// [TODO] 입력 받은 데이터를 어떻게 저장 할 것인가?
+// 
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
+	string log;
+
 	cout << "hello" << endl;
+	cout << "무엇을 하고 있나요? ";
+	cin >> log;
+	cout << endl << log << endl;
 
 	return 0;
 }
