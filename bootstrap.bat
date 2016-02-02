71c1ab4bdd43dd8d5f0b23d56c31828455fa3e7e
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2016-02-02 15:25:39 +0900

    handle errors

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,10 +1,24 @@
-git clone https://github.com/boostorg/boost.git boost
+@if not exist boost git clone https://github.com/boostorg/boost.git boost
 
 pushd "boost"
+
+:update_submodules
 git submodule update --init
+@if errorlevel 1 (
+	rem if using wifi, this can be failed.
+	@echo \"git submodule update\" was failed. Retry...
+	goto update_submodules
+)
+
 call "bootstrap.bat"
+@if errorlevel 1 exit /b %errorlevel%
+
 b2.exe toolset=msvc -q
+@if errorlevel 1 exit /b %errorlevel%
+
 b2.exe install --prefix=.\
+@if errorlevel 1 exit /b %errorlevel%
+
 popd
 
 call "build.bat"
